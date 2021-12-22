6bb22174328c65d30237dfa8218dfc95a6f6a53a
meh430 <mehulpillai30@gmail.com>
2021-12-22 18:06:34 -0500

    Adds or tests

--- test/OrTest.ts
+++ test/OrTest.ts
@@ -1,5 +1,6 @@
 import * as assert from "assert";
 import { Or } from "../src/language/Or";
+import { Not } from "../src/language/Not";
 import { Literal } from "../src/language/Literal";
 import { TestSuite } from "./test";
 
@@ -20,6 +21,20 @@ const tests: TestSuite = {
   "Or evaluates to 1 when both operands are 1": () => {
     const or = new Or(...getLiterals());
     assert.equal(or.evaluate({ p: true, q: true }), true);
+  },
+  "Or returns the correct formula": () => {
+    const p = new Literal("p");
+    const q = new Literal("q");
+    const r = new Literal("r");
+
+    assert.equal(new Or(p, q).getFormula(), "(p ∨ q)");
+    assert.equal(new Or(p, p).getFormula(), "(p ∨ p)");
+    assert.equal(new Or(p, q, r).getFormula(), "(p ∨ q ∨ r)");
+    assert.equal(new Or(p, new Not(q)).getFormula(), "(p ∨ (¬q))");
+    assert.equal(
+      new Or(p, q, r, r, p, q).getFormula(),
+      "(p ∨ q ∨ r ∨ r ∨ p ∨ q)"
+    );
   },
 };
 
