261655b78c09ba6c42dfd8bb9791f33d0413704e
meh430 <mehulpillai30@gmail.com>
2021-12-22 14:36:40 -0500

    Adds bc tests

--- test/BiconditionalTests.ts
+++ test/BiconditionalTests.ts
@@ -1,6 +1,7 @@
 import * as assert from "assert";
 import { Biconditional } from "../src/language/Biconditional";
 import { Literal } from "../src/language/Literal";
+import { Not } from "../src/language/Not";
 import { TestSuite } from "./test";
 
 function getBiconditional(): Biconditional {
@@ -14,6 +15,13 @@ const tests: TestSuite = {
     assert.equal(biconditional.evaluate({ p: true, q: false }), false);
     assert.equal(biconditional.evaluate({ p: false, q: true }), false);
     assert.equal(biconditional.evaluate({ p: true, q: true }), true);
+  },
+  "Biconditional returns the correct formula": () => {
+    const p = new Literal("p");
+    const q = new Literal("q");
+    assert.equal(new Biconditional(p, q).getFormula(), "(p ↔ q)");
+    assert.equal(new Biconditional(p, p).getFormula(), "(p ↔ p)");
+    assert.equal(new Biconditional(new Not(q), p).getFormula(), "((¬q) ↔ p)");
   },
 };
 
