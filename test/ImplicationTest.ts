de98f2c73b366acd388127d593d7d594aeb4bc7e
meh430 <mehulpillai30@gmail.com>
2021-12-22 17:59:05 -0500

    Adds implication tests

--- test/ImplicationTest.ts
+++ test/ImplicationTest.ts
@@ -1,5 +1,6 @@
 import * as assert from "assert";
 import { Implication } from "../src/language/Implication";
+import { Not } from "../src/language/Not";
 import { Literal } from "../src/language/Literal";
 import { TestSuite } from "./test";
 
@@ -16,6 +17,15 @@ const tests: TestSuite = {
   "Implication evaluates to 0 when antecedent is 1 and consequent is 0": () => {
     const implication = getImplication();
     assert.equal(implication.evaluate({ p: true, q: false }), false);
+  },
+  "Implication returns the correct formula": () => {
+    const p = new Literal("p");
+    const q = new Literal("q");
+
+    assert.equal(new Implication(p, q).getFormula(), "(p → q)");
+    assert.equal(new Implication(p, p).getFormula(), "(p → p)");
+    assert.equal(new Implication(new Not(p), q).getFormula(), "((¬p) → q)");
+    assert.equal(new Implication(p, new Not(q)).getFormula(), "(p → (¬q))");
   },
 };
 
