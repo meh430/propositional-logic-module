f966f5075bf6c824cfcd02f41d264bf9ed8a9271
meh430 <mehulpillai30@gmail.com>
2021-12-21 20:14:58 -0500

    Adds implication

---
+++ language/Implication.ts
@@ -0,0 +1,26 @@
+import { Formula } from "./Formula";
+
+export class Implication implements Formula {
+  antecedent: Formula;
+  consequent: Formula;
+
+  constructor(antecedent: Formula, consequent: Formula) {
+    this.antecedent = antecedent;
+    this.consequent = consequent;
+  }
+
+  getSymbols(): Set<string> {
+    return new Set<string>([...this.antecedent.getSymbols(), ...this.consequent.getSymbols()]);
+  }
+
+  evaluate(valuation: Map<string, boolean>): boolean {
+    const antecedentVal = this.antecedent.evaluate(valuation);
+    const consequentVal = this.consequent.evaluate(valuation);
+
+    return (!antecedentVal || consequentVal);
+  }
+
+  getFormula(): string {
+    return `(${this.antecedent.getFormula()} → ${this.consequent.getFormula()})`; 
+  }
+}
