2f91c33551e5adb9b82b28b61b1a9862aa13fa8d
meh430 <mehulpillai30@gmail.com>
2021-12-22 10:57:41 -0500

    Adds contradiction

--- src/Contradiction.ts
+++ src/Contradiction.ts
@@ -1,8 +1,8 @@
 import { Formula } from "./language/Formula";
 import { generateValuations } from "./Utils";
 
-export function isTautology(formula: Formula): boolean {
+export function isContradiction(formula: Formula): boolean {
   const symbols = formula.getSymbols();
   const valuations = generateValuations(symbols);
-  return !valuations.some(v => !formula.evaluate(v));
+  return !valuations.some(v => formula.evaluate(v));
 }
---
+++ test/ContradictionTest.ts
@@ -0,0 +1,42 @@
+import * as assert from "assert";
+import { isContradiction } from "../src/Contradiction";
+import { Literal } from "../src/language/Literal";
+import { Or } from "../src/language/Or";
+import { And } from "../src/language/And";
+import { Not } from "../src/language/Not";
+import { Implication } from "../src/language/Implication";
+import { TestSuite } from "./test";
+
+const tests: TestSuite = {
+  "Contradiction 1": () => {
+    // p and not p
+    const p = new Literal("p");
+    const contradiction = new And(p, new Not(p));
+    assert.equal(isContradiction(contradiction), true);
+  },
+  "Contradiction 2": () => {
+    // not (((p implies q) and p) implies q)
+    const p = new Literal("p");
+    const q = new Literal("q");
+    const contradiction = new Implication(new And(new Implication(p, q), p), q);
+    assert.equal(isContradiction(new Not(contradiction)), true);
+  },
+  "Not a contradiction 1": () => {
+    // p or not p
+    const p = new Literal("p");
+    const tautology = new Or(p, new Not(p));
+    assert.equal(isContradiction(tautology), false);
+  },
+  "Not a contradiction 2": () => {
+    const p = new Literal("p");
+    assert.equal(isContradiction(p), false);
+  },
+  "Not a contradiction 3": () => {
+    const p = new Literal("p");
+    const q = new Literal("q");
+    const contingent = new Implication(new Or(p, q), new Or(p, new Not(q)));
+    assert.equal(isContradiction(contingent), false);
+  },
+};
+
+module.exports = tests;
