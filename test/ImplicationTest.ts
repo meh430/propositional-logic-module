2e4a2e53f96a205877f9ce6b96b75cfa3dae3ad6
meh430 <mehulpillai30@gmail.com>
2021-12-22 10:02:13 -0500

    Adds impl tests

---
+++ test/ImplicationTest.ts
@@ -0,0 +1,22 @@
+import * as assert from "assert";
+import { Implication } from "../language/Implication";
+import { Literal } from "../language/Literal";
+import { TestSuite } from "./test";
+
+function getImplication(): Implication {
+  return new Implication(new Literal("p"), new Literal("q"));
+}
+
+const tests: TestSuite = {
+  "Implication evaluates to 1 when antecedent is 0": () => {
+    const implication = getImplication();
+    assert.equal(implication.evaluate({ p: false, q: false }), true);
+    assert.equal(implication.evaluate({ p: false, q: true }), true);
+  },
+  "Implication evaluates to 0 when antecedent is 1 and consequent is 0": () => {
+    const implication = getImplication();
+    assert.equal(implication.evaluate({ p: true, q: false }), false);
+  }
+};
+
+module.exports = tests;
